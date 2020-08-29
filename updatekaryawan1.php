0ce06ba79b0066cb4006e9c1bcbd384c1796682b
Alvitariani Khairinia Kusuma <48576002+alvitarianikk@users.noreply.github.com>
2020-08-29 15:06:11 +0700

    Update updatekaryawan1.php

--- updatekaryawan1.php
+++ updatekaryawan1.php
@@ -10,33 +10,19 @@ $row        = mysqli_fetch_array($logbook);
         <title>Update Data</title>
     </head>
     <body>
+
 	<form method="post" action="updatekaryawan2.php">
 	
  <input type="hidden" value="<?php echo $row['id'];?>" name="id">
 <table>
 
 <tr><td><font color="#000000">Damage Date</td><td>:</td><td><input type="text" name="tanggal" size="15" maxlength="50" value="<?php echo $row['tanggal'];?>"></td>
-<td><select name="bulan" value="<?php echo $row['bulan'];?>">
-<option value="January">January</option>
-<option value="February">February</option>
-<option value="March">March</option>
-<option value="April">April</option>
-<option value="May">May</option>
-<option value="June">June</option>
-<option value="July">July</option>
-<option value="August">August</option>
-<option value="September">September</option>
-<option value="October">October</option>
-<option value="November">November</option>
-<option value="December">December</option></td>
-</select>
-<td><font color="#000000">Year : <input type="text" name="tahun" size="15" maxlength="50" value="<?php echo $row['tahun'];?>">
-</tr>
+
 <tr><td><font color="#000000">Damage Time</td><td>:</td><td><input type="text" name="waktuindo" size="35" maxlength="50" value="<?php echo $row['waktuindo'];?>"></td></tr>
 
 <tr><td><font color="#000000">Normal Date</td><td>:</td><td><input type="text" name="tanggalnormal" size="35" maxlength="50" value="<?php echo $row['tanggalnormal'];?>"></td></tr>
 <tr><td><font color="#000000">Normal Time</td><td>:</td><td><input type="text" name="waktuus" size="35" maxlength="50" value="<?php echo $row['waktuus'];?>"></td></tr>
-<tr><td><font color="#000000">Name</td><td>:</td><td><input type="text" name="nama" size="35" maxlength="50" value=""<?php echo $row['nama'];?>""></td></tr>
+<tr><td><font color="#000000">Name</td><td>:</td><td><input type="text" name="nama" size="35" maxlength="50" value="<?php echo $row['nama'];?>"></td></tr>
 <tr><td><font color="#000000">Facility</td><td>:</td><td><select name="fasilitas" value="<?php echo $row['fasilitas'];?>">
 <option value="Communication">Communication</option>
 <option value="Navigation">Navigation</option>
@@ -44,9 +30,9 @@ $row        = mysqli_fetch_array($logbook);
 <option value="Automation">Automation</option></tr></td></select>
 <tr><td><font color="#000000">Equipment</td><td>:</td><td><input type="text" name="peralatan" size="35" maxlength="50" value="<?php echo $row['peralatan'];?>"></td></tr>
 <tr><td><font color="#000000">Merk Type</td><td>:</td><td><input type="text" name="merk" size="35" maxlength="50" value="<?php echo $row['merk'];?>"></td></tr>
-<tr><td><font color="#000000">Parts of Equipment</td><td>:</td><td><textarea name="bagianalat" rows="5" cols="30" value="<?php echo $row['bagianalat'];?>"></textarea></tr></td>
-<tr><td><font color="#000000">Description</td><td>:</td><td><textarea name="detailrusak" rows="5" cols="30" value="<?php echo $row['detailrusak'];?>"></textarea></tr></td>
-<tr><td><font color="#000000">Repair</td><td>:</td><td><textarea name="perbaikan" rows="5" cols="30" value="<?php echo $row['perbaikan'];?>"></textarea></td></tr>
+<tr><td><font color="#000000">Parts of Equipment</td><td>:</td><td><input type="text" name="bagianalat" size="85" maxlength="500" value="<?php echo $row['bagianalat'];?>"></textarea></tr></td>
+<tr><td><font color="#000000">Description</td><td>:</td><td><input type="text" name="detailrusak" size="85" maxlength="500" value="<?php echo $row['detailrusak'];?>"></textarea></tr></td>
+<tr><td><font color="#000000">Repair</td><td>:</td><td><input type="text" name="perbaikan" size="85" maxlength="500" value="<?php echo $row['perbaikan'];?>"></textarea></td></tr>
 
 <tr><td><font color="#000000">Status</td><td>:</td><td><input type="hidden" value="<?php echo $row['status'];?>" name="status"></td></tr></p>
                 <tr><td colspan="2"><button type="submit" value="simpan">SIMPAN PERUBAHAN</button></td></tr>
